cc29dddfcd0f7170d660030adb57ff7ca5ce962e
Akshay Gaikwad <101447868+argaikwad96@users.noreply.github.com>
2025-11-26 17:10:49 +0530

    Update math_ops.py

---
+++ src/math_ops.py
@@ -0,0 +1,15 @@
+# src/math_ops.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("Addition:", add(5, 3))
+    print("Subtraction:", subtract(5, 3))
+    print("Multiplication:", multiply(5, 3))
